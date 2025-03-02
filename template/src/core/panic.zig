8754354c4d23a0451b04b27268fdc87c1c3920d5
Vladislav Fursov <github@ghost.im>
2025-03-02 06:49:15 +0400

    template: core: panic: enable led on hang.

--- template/src/core/panic.zig
+++ template/src/core/panic.zig
@@ -50,7 +50,12 @@ pub fn hang() noreturn {
 
     // Fast blink for debugging.
     // FIXME: use LED GPIO instead raw.
-    const GPIOC_OUTDR: *volatile u32 = @ptrFromInt(0x40011000 + 0x0C);
+    const GPIOC_BASE: u32 = 0x40011000;
+    const GPIOC_CFGLR: *volatile u32 = @ptrFromInt(GPIOC_BASE + 0x00);
+    const GPIOC_OUTDR: *volatile u32 = @ptrFromInt(GPIOC_BASE + 0x0C);
+    GPIOC_CFGLR.* &= ~@as(u32, 0b1111 << 0); // Clear all bits for PC0
+    GPIOC_CFGLR.* |= @as(u32, 0b0011 << 0); // Set push-pull output for PC0
+
     while (true) {
         GPIOC_OUTDR.* ^= @as(u16, 1 << 0); // Toggle PC0
 
