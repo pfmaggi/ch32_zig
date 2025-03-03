f53653bf3b114b4844ee6ce9ed9da34979692645
Vladislav Fursov <github@ghost.im>
2025-03-03 04:22:36 +0400

    tools/svd4zig: add build test step

--- tools/svd4zig/build.zig
+++ tools/svd4zig/build.zig
@@ -12,4 +12,14 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
     b.installArtifact(exe);
+
+    const test_step = b.step("test", "Run tests");
+    const unit_tests = b.addTest(.{
+        .name = "svd4zig",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const unit_tests_run = b.addRunArtifact(unit_tests);
+    test_step.dependOn(&unit_tests_run.step);
 }
