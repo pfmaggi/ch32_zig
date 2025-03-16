fb26faa9c6bb457a8273e522b3cf2286237c2f05
Vladislav Fursov <github@ghost.im>
2025-03-17 00:17:07 +0400

    template: hal: deadline: quick fix

--- template/src/hal/deadline.zig
+++ template/src/hal/deadline.zig
@@ -4,12 +4,22 @@ const svd = @import("svd");
 
 pub const DeadlineFn = fn () bool;
 
+// FIXME: refactoring is needed because the type remembers the number and
+//  the counter does not reset on subsequent calls.
 pub fn simple(count: u32) ?DeadlineFn {
     return struct {
         var counter = count;
+        pub fn reset() void {
+            counter = count;
+        }
         pub fn check() bool {
+            if (counter == 0) {
+                reset();
+                return true;
+            }
+
             counter -= 1;
-            return counter == 0;
+            return false;
         }
     }.check;
 }
