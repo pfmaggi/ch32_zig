a67d1ed6f227c86a3b583e13fd4ce746dc7226c0
Vladislav Fursov <github@ghost.im>
2025-03-28 06:01:01 +0400

    examples: update led switch

--- examples/blink/src/main.zig
+++ examples/blink/src/main.zig
@@ -6,10 +6,10 @@ pub fn main() !void {
     // Select LED pin based on chip series.
     const led = switch (config.chip.series) {
         .ch32v003 => hal.Pin.init(.GPIOC, 0),
-        // .ch32v103 => hal.Pin.init(.GPIOC, 0),
+        .ch32v103 => hal.Pin.init(.GPIOC, 0),
         .ch32v20x => hal.Pin.init(.GPIOA, 15), // nanoCH32V203 board
         .ch32v30x => hal.Pin.init(.GPIOA, 3), // nanoCH32V305 board
-        else => @compileError("Unsupported chip series"),
+        // else => @compileError("Unsupported chip series"),
     };
     hal.port.enable(led.port);
     // hal.port.disable(led.port);
