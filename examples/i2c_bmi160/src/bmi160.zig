3e56f5d170cf533bdfbccc7392144a82ec385b06
Vladislav Fursov <github@ghost.im>
2025-03-30 02:25:12 +0400

    examples: i2c bmi160: add

---
+++ examples/i2c_bmi160/src/bmi160.zig
@@ -0,0 +1,81 @@
+const std = @import("std");
+const hal = @import("hal");
+
+// https://github.com/LiquidCGS/FastIMU/blob/main/src/F_BMI160.cpp
+pub fn setup(I2C1: hal.I2c, addr: u7, deadlineFn: ?hal.I2c.DeadlineFn) !void {
+    const BMI160_CHIP_ID = 0x00;
+    const BMI160_CHIP_ID_DEFAULT_VALUE = 0xD1;
+
+    const BMI160_RA_CMD = 0x7E;
+
+    const BMI160_GYR_RANGE = 0x43;
+    const BMI160_GYR_CONF = 0x42;
+    const BMI160_ACC_RANGE = 0x41;
+    const BMI160_ACC_CONF = 0x40;
+
+    // Ping device example.
+    I2C1.masterTransferBlocking(.from7(addr), null, null, deadlineFn) catch |err| switch (err) {
+        error.AckFailure => return error.I2CDeviceNotFound,
+        else => return err,
+    };
+
+    // Request CHIP_ID.
+    var chip_id_buf = [_]u8{0x00};
+    try I2C1.masterTransferBlocking(.from7(addr), &.{BMI160_CHIP_ID}, &chip_id_buf, deadlineFn);
+    // Check CHIP_ID.
+    if (chip_id_buf[0] != BMI160_CHIP_ID_DEFAULT_VALUE) {
+        return error.I2CDeviceNotBMI160;
+    }
+
+    // Soft-reset device.
+    try I2C1.masterTransferBlocking(.from7(addr), &.{ BMI160_RA_CMD, 0xB6 }, null, deadlineFn);
+    hal.delay.ms(100);
+
+    // Power up the accelerometer
+    try I2C1.masterTransferBlocking(.from7(addr), &.{ BMI160_RA_CMD, 0x11 }, null, deadlineFn);
+    hal.delay.ms(100);
+
+    // Power up the gyroscope
+    try I2C1.masterTransferBlocking(.from7(addr), &.{ BMI160_RA_CMD, 0x15 }, null, deadlineFn);
+    hal.delay.ms(100);
+
+    // Set up full-scale range for the accelerometer. 0x0C = 16g
+    try I2C1.masterTransferBlocking(.from7(addr), &.{ BMI160_ACC_RANGE, 0x0C }, null, deadlineFn);
+    // Set up full-scale range for the gyroscope. 0x00 = 2000dps
+    try I2C1.masterTransferBlocking(.from7(addr), &.{ BMI160_GYR_RANGE, 0x00 }, null, deadlineFn);
+
+    // Set Accel ODR to 400hz, BWP mode to Oversample 4, LPF of ~40.5hz
+    try I2C1.masterTransferBlocking(.from7(addr), &.{ BMI160_ACC_CONF, 0x0A }, null, deadlineFn);
+    // Set Gyro ODR to 400hz, BWP mode to Oversample 4, LPF of ~34.15hz
+    try I2C1.masterTransferBlocking(.from7(addr), &.{ BMI160_GYR_CONF, 0x0A }, null, deadlineFn);
+}
+
+pub const ImuData = packed struct {
+    gyr_x: i16,
+    gyr_y: i16,
+    gyr_z: i16,
+    acc_x: i16,
+    acc_y: i16,
+    acc_z: i16,
+
+    pub fn format(self: ImuData, comptime _: []const u8, _: std.fmt.FormatOptions, out_stream: anytype) !void {
+        _ = try out_stream.print("gyro: {d:>5} | {d:>5} | {d:>5} |     accel: {d:>5} | {d:>5} | {d:>5}", .{
+            self.gyr_x,
+            self.gyr_y,
+            self.gyr_z,
+            self.acc_x,
+            self.acc_y,
+            self.acc_z,
+        });
+    }
+};
+
+pub fn readImuData(I2C1: hal.I2c, addr: u7, imu_data_ret: *ImuData, deadlineFn: ?hal.I2c.DeadlineFn) !void {
+    // const a_res = 16.0 / 32768.0; // value for full range (16g) readings
+    // const g_res = 2000.0 / 32768.0; // value for full range (2000dps) readings
+
+    // Read the 12 raw data registers into data array
+    const BMI160_GYR_X_L = 0x0C;
+    // Read directly to the structure, thanks `std.mem.asBytes`.
+    try I2C1.masterTransferBlocking(.from7(addr), &.{BMI160_GYR_X_L}, std.mem.asBytes(imu_data_ret), deadlineFn);
+}
---
+++ examples/i2c_bmi160/src/main.zig
@@ -0,0 +1,72 @@
+const std = @import("std");
+const config = @import("config");
+const hal = @import("hal");
+
+const bmi160 = @import("bmi160.zig");
+
+pub const panic = hal.panic.log;
+pub const std_options: std.Options = .{
+    .log_level = .debug,
+    .logFn = hal.log.logFn,
+};
+
+pub fn main() !void {
+    const clock = hal.clock.setOrGet(.hse_max);
+    hal.delay.init(clock);
+
+    // Configure UART for logging.
+
+    const USART1 = hal.Uart.init(.USART1, .{});
+    USART1.configureBaudRate(.{
+        .peripheral_clock = switch (config.chip.series) {
+            .ch32v003 => clock.hb,
+            else => clock.pb2,
+        },
+        .baud_rate = 115_200,
+    });
+    hal.log.setWriter(USART1.writer().any());
+
+    // Configure I2C.
+
+    const I2C1 = hal.I2c.init(.I2C1, .{
+        // You can use the default pins for the selected I2C by leaving the field null or remap it to other pins.
+        // For example, to use the remapped pins for I2C1 on CH32V003:
+        // .pins = hal.I2c.Pins.i2c1.scl_pd1_sda_pd0,
+    });
+    // You can deinitialize the I2C peripheral if you don't need it anymore.
+    defer I2C1.deinit();
+
+    // Runtime baud rate configuration.
+    try I2C1.configureBaudRate(.{
+        .peripheral_clock = switch (config.chip.series) {
+            .ch32v003 => clock.hb,
+            else => clock.pb2,
+        },
+        .speed = 400_000,
+        .duty_cycle = .@"16/9",
+    });
+
+    // Can be 0x68 or 0x69.
+    const BMI160_I2C_ADDR = 0x69;
+
+    bmi160.setup(I2C1, BMI160_I2C_ADDR, null) catch |err| switch (err) {
+        error.I2CDeviceNotFound => {
+            std.log.err("Device on address 0x{X:0>2} not found", .{BMI160_I2C_ADDR});
+            return err;
+        },
+        error.I2CDeviceNotBMI160 => {
+            std.log.err("Device on address 0x{X:0>2} is not BMI160", .{BMI160_I2C_ADDR});
+            return err;
+        },
+        else => return err,
+    };
+
+    while (true) {
+        var imu_data: bmi160.ImuData = undefined;
+        try bmi160.readImuData(I2C1, BMI160_I2C_ADDR, &imu_data, null);
+
+        std.log.info("{any}", .{imu_data});
+
+        hal.delay.ms(1000);
+    }
+}
