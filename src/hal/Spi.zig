c33d5a06f048190862e27e8ff414e9f717444872
Vladislav Fursov <github@ghost.im>
2025-03-30 23:19:49 +0400

    hal: SPI: remove noinline attr

--- src/hal/Spi.zig
+++ src/hal/Spi.zig
@@ -280,7 +280,7 @@ fn reset(self: SPI) void {
 ///
 /// This function blocks execution until all data is transferred or a timeout occurs.
 /// The deadlineFn parameter allows specifying a timeout check function to avoid infinite waiting.
-pub noinline fn transferBlocking(self: SPI, comptime u8_or_u16: type, send: ?[]const u8_or_u16, recv: ?[]u8_or_u16, deadlineFn: ?DeadlineFn) Timeout!void {
+pub fn transferBlocking(self: SPI, comptime u8_or_u16: type, send: ?[]const u8_or_u16, recv: ?[]u8_or_u16, deadlineFn: ?DeadlineFn) Timeout!void {
     const type_info = @typeInfo(u8_or_u16).int;
     if (type_info.bits != 8 and type_info.bits != 16 or @typeInfo(u8_or_u16).int.signedness != .unsigned) {
         @compileError("Unsupported type, only u8 and u16 are supported");
