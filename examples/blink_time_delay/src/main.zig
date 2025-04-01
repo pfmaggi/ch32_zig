1ed662d97f426e372e15f11e59817d44edd18e8a
Vladislav Fursov <github@ghost.im>
2025-04-01 04:44:04 +0400

    examples: blink_time_delay: add

---
+++ examples/blink_time_delay/src/main.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const config = @import("config");
+const hal = @import("hal");
+
+pub const interrupts: hal.interrupts.VectorTable = .{ .SysTick = hal.time.sysTickHandler };
+
+pub fn main() !void {
+    hal.time.init(.default);
+
+    // Select LED pin based on chip series.
+    const led = switch (config.chip.series) {
+        .ch32v003 => hal.Pin.init(.GPIOC, 0),
+        .ch32v103 => hal.Pin.init(.GPIOC, 0),
+        .ch32v20x => hal.Pin.init(.GPIOA, 15), // nanoCH32V203 board
+        .ch32v30x => hal.Pin.init(.GPIOA, 3), // nanoCH32V305 board
+        // else => @compileError("Unsupported chip series"),
+    };
+    hal.port.enable(led.port);
+    // hal.port.disable(led.port);
+
+    led.asOutput(.{ .speed = .max_50mhz, .mode = .push_pull });
+
+    while (true) {
+        // const on = led.read();
+        // led.write(!on);
+        // or
+        led.toggle();
+
+        hal.time.delay.ms(1000);
+    }
+}
