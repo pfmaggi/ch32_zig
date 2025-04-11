8938e279d594d3a8145a06865419e088f2b9a8fc
Vladislav Fursov <github@ghost.im>
2025-04-11 06:56:04 +0400

    examples: replace uart_logger_panic by uart_logger because panic is now always handled if a logger is set.

--- examples/uart_logger/src/main.zig
+++ examples/uart_logger/src/main.zig
@@ -5,7 +5,19 @@ const ch32 = @import("ch32");
 
 pub const ch32_options: ch32.Options = .{
     .log_level = .debug,
+    // When a logger function other than nopFn is set,
+    // it will be used to output the panic message.
     .logFn = hal.log.logFn,
+    // Also we can set the panic options with a LED pin to blink on panic.
+    .panic_options = .{
+        .led = switch (config.chip.series) {
+            .ch32v003 => hal.Pin.init(.GPIOC, 0),
+            .ch32v103 => hal.Pin.init(.GPIOC, 0),
+            .ch32v20x => hal.Pin.init(.GPIOA, 15), // nanoCH32V203 board
+            .ch32v30x => hal.Pin.init(.GPIOA, 3), // nanoCH32V305 board
+            // else => @compileError("Unsupported chip series"),
+        },
+    },
 };
 
 pub fn main() !void {
@@ -42,6 +54,11 @@ pub fn main() !void {
 
         // Print counter to UART.
         std.log.info("Counter: {}", .{counter});
+
+        if (counter == 10) {
+            // Trigger a panic.
+            @panic("Counter reached 10");
+        }
 
         hal.delay.ms(1000);
     }
